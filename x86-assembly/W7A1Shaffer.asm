86d8d67de235f30f214ab19e5d4003da30713567
Lucas Shaffer <shafferlucas@gmail.com>
2017-12-17 13:51:06 -0800

    Edited for better readability.

--- x86-assembly/W7A1Shaffer.asm
+++ x86-assembly/W7A1Shaffer.asm
@@ -14,19 +14,19 @@ arr3 SDWORD -6412, -4658, -6415, -54, -6785, -68759, -6665, -4654, -4065
 .code
 main PROC
     
-    invoke  FindLargest, ADDR arr1, LENGTHOF arr1	;Invoke FindLargest PROC and pass ADDR for arr1 and LENGTHOF arr1
-    call	WriteInt								;Display the largest Int
-    call	Crlf									;Write New Line
+    invoke  FindLargest, ADDR arr1, LENGTHOF arr1  ;Invoke FindLargest PROC and pass ADDR for arr1 and LENGTHOF arr1
+    call	WriteInt                               ;Display the largest Int
+    call	Crlf                                   ;Write New Line
 
-    invoke  FindLargest, ADDR arr2, LENGTHOF arr2	;Invoke FindLargest PROC and pass ADDR for arr2 and LENGTHOF arr2
-    call	WriteInt								;Display the largest Int
-    call	Crlf									;Write New Line
+    invoke  FindLargest, ADDR arr2, LENGTHOF arr2  ;Invoke FindLargest PROC and pass ADDR for arr2 and LENGTHOF arr2
+    call	WriteInt                               ;Display the largest Int
+    call	Crlf                                   ;Write New Line
   
-    invoke  FindLargest, ADDR arr3, LENGTHOF arr3	;Invoke FindLargest PROC and pass ADDR for arr3 and LENGTHOF arr3
-    call	WriteInt								;Display the largest Int
-    call	Crlf									;Write New Line
+    invoke  FindLargest, ADDR arr3, LENGTHOF arr3  ;Invoke FindLargest PROC and pass ADDR for arr3 and LENGTHOF arr3
+    call	WriteInt                               ;Display the largest Int
+    call	Crlf                                   ;Write New Line
 
-    call	WaitMsg									;Write wait message
+    call	WaitMsg                                ;Write wait message
 
     exit						 
 main ENDP
@@ -34,19 +34,19 @@ main ENDP
 FindLargest PROC USES esi ecx,
     array:SDWORD, len:BYTE
     
-    mov	  esi,array     ;Move the address value in array to esi
-    mov	  eax,[esi]     ;Move the value at the esi address to eax
-    movzx ecx,len	    ;Move len to ecx
-    dec	  ecx	        ;Decriment ecx
+    mov	  esi,array  ;Move the address value in array to esi
+    mov	  eax,[esi]  ;Move the value at the esi address to eax
+    movzx ecx,len    ;Move len to ecx
+    dec	  ecx        ;Decriment ecx
 L1:
-    add	  esi,4	        ;Add 4 to esi to move to next value
-    cmp	  [esi],eax     ;Compare the current value to eax
-    jg	  L2		    ;Jump to L2 if the current value is greater than eax
-    jmp	  L3		    ;Jump to L3 if its not greater
+    add	  esi,4      ;Add 4 to esi to move to next value
+    cmp	  [esi],eax  ;Compare the current value to eax
+    jg	  L2         ;Jump to L2 if the current value is greater than eax
+    jmp	  L3         ;Jump to L3 if its not greater
 L2:
-    mov   eax,[esi]	    ;Move the current value to eax as the newest largest value
+    mov   eax,[esi]  ;Move the current value to eax as the newest largest value
 L3:
-    loop  L1		    ;Loop to L1
+    loop  L1         ;Loop to L1
     ret
 FindLargest ENDP
 
