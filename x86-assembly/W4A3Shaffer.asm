867ab69ed99628654fe86da1760202589494734c
Lucas Shaffer <shafferlucas@gmail.com>
2017-12-17 13:42:18 -0800

    Edited for better readability.

--- x86-assembly/W4A3Shaffer.asm
+++ x86-assembly/W4A3Shaffer.asm
@@ -7,46 +7,46 @@ TITLE Week 4 Assignment 3 Shaffer
 INCLUDE Irvine32.inc
 
 .data
-text		DWORD ?		;To hold text color
-background	DWORD ?		;To hold background color
+text		DWORD ?     ;To hold text color
+background	DWORD ?     ;To hold background color
 
 .code
 main PROC
     mov   background,0h ;Set background to 0(black)
-    mov	  ecx,16		;Move 16 to ecx for outer loop control
+    mov	  ecx,16        ;Move 16 to ecx for outer loop control
 L1:
     
-    push  ecx		    ;Push ecx to the stack
-    mov	  ecx,16		;Move 16 to ecx for inner loop control
-    mov	  text,0h		;Set text to 0(black)
-    shl	  background,4	;Move the background value left 4 bits
+    push  ecx           ;Push ecx to the stack
+    mov	  ecx,16        ;Move 16 to ecx for inner loop control
+    mov	  text,0h       ;Set text to 0(black)
+    shl	  background,4  ;Move the background value left 4 bits
 L2:
     
-    mov	  eax,text		 ;Move text value to ex
+    mov	  eax,text       ;Move text value to ex
     add	  eax,background ;Add the background value to eax
-    call  SetTextColor	 ;Call SetTextColor
+    call  SetTextColor   ;Call SetTextColor
     
-    push  eax		    ;Push eax onto the stack
+    push  eax           ;Push eax onto the stack
 
-    mov	  al,'X'		;Move 'X' to al
-    call  WriteChar	    ;Call WriteChar
+    mov	  al,'X'        ;Move 'X' to al
+    call  WriteChar     ;Call WriteChar
 
-    pop	  eax		    ;Pop eax off of the stack
+    pop	  eax           ;Pop eax off of the stack
 
-    inc	  text			;Increment text value
-    loop  L2			;Return to the top of the inner loop L2
+    inc	  text          ;Increment text value
+    loop  L2            ;Return to the top of the inner loop L2
 
-    call  Crlf			;Move to a new line
-    pop	  ecx			;Pop ecx off of the stack
+    call  Crlf          ;Move to a new line
+    pop	  ecx           ;Pop ecx off of the stack
 
-    shr	  background,4	;Move background value right 4 bits for inc
-    inc	  background	;Increment the background value
-    loop  L1			;Return to the top of the outer loop L1
+    shr	  background,4  ;Move background value right 4 bits for inc
+    inc	  background    ;Increment the background value
+    loop  L1            ;Return to the top of the outer loop L1
 
-    mov	  eax,7			;Move 7 to eax for white on black text
-    call  SetTextColor	;Call SetTextColor
+    mov	  eax,7         ;Move 7 to eax for white on black text
+    call  SetTextColor  ;Call SetTextColor
 
-    call  WaitMsg		;Display a wait message
+    call  WaitMsg       ;Display a wait message
 
     exit
 main ENDP
